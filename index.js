05e922fd80739e088e198cfa8ca184b8f23948be
Pedro Henrique Cardozo Ferreira <p-cardozo@hotmail.com>
2020-07-20 13:43:44 -0300

    Add Querying Documents and Adding Documents

--- index.js
+++ index.js
@@ -12,3 +12,28 @@ const courseSchema = new mongoose.Schema({
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
 });
+
+const Course = mongoose.model("Course", courseSchema);
+
+async function createCourse() {
+  const course = new Course({
+    name: "React Course",
+    author: "Pedro",
+    tags: ["react", "front-end"],
+    isPublished: true,
+  });
+
+  const result = await course.save();
+  console.log(result);
+}
+
+async function getCourses() {
+  const courses = await Course
+    .find({ author: "Pedro", isPublished: true })
+    .limit(10)
+    .sort({ name: -1 })
+    .select({ name: 1, tags: 1 });
+  console.log(courses);
+}
+
+getCourses();
